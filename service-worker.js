ef560ff7a6d96c42dc2bc0e0dda25459bc59d650
yuao-188 <yuao-188@users.noreply.github.com>
2026-09-17 18:16:44 +0800

    Finalize thermal viewer checks

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fuelcell-propulsion-v5-thermal-1";
+const CACHE_NAME = "fuelcell-propulsion-v5-thermal-2";
 
 async function discoverCoreAssets() {
   const response = await fetch("./index.html", { cache: "no-cache" });
